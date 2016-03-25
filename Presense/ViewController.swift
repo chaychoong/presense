3a95b3b3dc2d1cbfaada81db7a11c60bb8675027
Chay Choong <chaychoong@gmail.com>
2016-03-26 02:23:34 +0800

    Fixed memory retrieval and added memory structure for status

--- Presense/ViewController.swift
+++ Presense/ViewController.swift
@@ -19,16 +19,37 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var nearestField: UILabel!
+    
+    @IBOutlet weak var availableButton: UIButton!
+    @IBOutlet weak var busyButton: UIButton!
+    
     @IBAction func testButton(sender: AnyObject) {
         saveName(nameField.text!, url: textField.text!)
         sendMessage("\((identity!.valueForKey("name") as? String)!) registered under \((identity!.valueForKey("major") as? NSNumber)!):\((identity!.valueForKey("minor") as? NSNumber)!)")
+        self.view.backgroundColor = UIColor(red: CGFloat(98)/255.0, green: CGFloat(177)/255.0, blue: CGFloat(126)/255.0, alpha: 1.0)
+        availableButton.enabled = false
+        saveStatus("available")
+    }
+    
+    @IBAction func availableButton(sender: AnyObject) {
+        availableButton.enabled = false
+        busyButton.enabled = true
+        sendMessage("\((identity!.valueForKey("name") as? String)!) is available")
+        self.view.backgroundColor = UIColor(red: CGFloat(98)/255.0, green: CGFloat(177)/255.0, blue: CGFloat(126)/255.0, alpha: 1.0)
+        saveStatus("available")
+    }
+    
+    @IBAction func busyButton(sender: AnyObject) {
+        busyButton.enabled = false
+        availableButton.enabled = true
+        sendMessage("\((identity!.valueForKey("name") as? String)!) is busy")
+        self.view.backgroundColor = UIColor(red: CGFloat(58)/255.0, green: CGFloat(145)/255.0, blue: CGFloat(219)/255.0, alpha: 1.0)
+        saveStatus("busy")
     }
     
     var beaconManager:ESTBeaconManager!
     var nearestBeacon:CLBeacon!
     var beaconFound = false
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,18 +72,33 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
         let fetchRequest = NSFetchRequest(entityName: "SlackData")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+            fetchRequest.fetchLimit = 1
+        
         do {
             let result =
                 try managedContext.executeFetchRequest(fetchRequest)
-            if (result.count != 0) {
+            if (result.count > 0) {
                 identity = result[0] as? NSManagedObject
-                nameLabel.text = "Name set to: \((identity!.valueForKey("name") as? String)!)"
-                urlLabel.text = "URL set to: \((identity!.valueForKey("url") as? String)!)"
-                nearestField.text = "Nearest Beacon: \((identity!.valueForKey("major") as? NSNumber)!):\((identity!.valueForKey("minor") as? NSNumber)!)"
+                
+                nameLabel.text = "Name set to: \(identity!.valueForKey("name") as! String)"
+                urlLabel.text = "URL set to: \(identity!.valueForKey("url") as! String)"
+                nearestField.text = "Nearest Beacon: \(identity!.valueForKey("major") as! NSNumber):\(identity!.valueForKey("minor") as! NSNumber)"
+                if ((identity!.valueForKey("status") as! String) == "busy") {
+                    self.view.backgroundColor = UIColor(red: CGFloat(58)/255.0, green: CGFloat(145)/255.0, blue: CGFloat(219)/255.0, alpha: 1.0)
+                }
+                else {
+                    self.view.backgroundColor = UIColor(red: CGFloat(98)/255.0, green: CGFloat(177)/255.0, blue: CGFloat(126)/255.0, alpha: 1.0)
+                }
             }
         } catch let error as NSError {
             print("Could not fetch \(error), \(error.userInfo)")
         }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
     }
     
     func saveName(name: String, url: String) {
@@ -72,14 +108,16 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
         let entity =  NSEntityDescription.entityForName("SlackData", inManagedObjectContext:managedContext)
         
         let fetchRequest = NSFetchRequest(entityName: "SlackData")
-//        fetchRequest.predicate = NSPredicate(format: "name == %@", (identity!.valueForKey("name") as? String)!)
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+            fetchRequest.fetchLimit = 1
+        
         var webhookURL = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
         
         do {
             let result =
                 try managedContext.executeFetchRequest(fetchRequest)
-            if (result.count != 0) {
-                webhookURL = (result[0] as? NSManagedObject)!
+            if (result.count > 0) {
+                webhookURL = result[0] as! NSManagedObject
                 
                 self.beaconManager.stopMonitoringForRegion(CLBeaconRegion(
                     proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!,
@@ -94,6 +132,7 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
         
         webhookURL.setValue(url, forKey: "url")
         webhookURL.setValue(name, forKey: "name")
+        webhookURL.setValue(NSDate(), forKey: "date")
         
         if beaconFound {
             webhookURL.setValue(nearestBeacon.major, forKey: "major")
@@ -101,7 +140,7 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
         }
         
         do {
-            try managedContext.save()
+            try webhookURL.managedObjectContext?.save()
             
             identity = webhookURL
             
@@ -119,6 +158,38 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
             else {
                 nearestField.text = "Nearest Beacon: Undetected"
             }
+            
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    func saveStatus(status: String) {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity =  NSEntityDescription.entityForName("SlackData", inManagedObjectContext:managedContext)
+        let fetchRequest = NSFetchRequest(entityName: "SlackData")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+            fetchRequest.fetchLimit = 1
+        var webhookURL = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        do {
+            let result =
+                try managedContext.executeFetchRequest(fetchRequest)
+            if (result.count > 0) {
+                webhookURL = result[0] as! NSManagedObject
+            }
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        
+        webhookURL.setValue(status, forKey: "status")
+        
+        do {
+            try webhookURL.managedObjectContext?.save()
+            
+            identity = webhookURL
             
         } catch let error as NSError  {
             print("Could not save \(error), \(error.userInfo)")
